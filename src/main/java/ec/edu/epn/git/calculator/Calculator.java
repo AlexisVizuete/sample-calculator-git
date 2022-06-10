7ae9837cb05cd12719c7240c233d1f450a5a2bfe
Alexis Vizuete <50114642+alexisvc@users.noreply.github.com>
2022-06-10 18:56:34 -0500

    Añadimos la clase y los tests unitarios de la calculadora

---
+++ src/main/java/ec/edu/epn/git/calculator/Calculator.java
@@ -0,0 +1,36 @@
+package ec.edu.epn.git.calculator;
+
+public class Calculator {
+
+    private int respuesta;
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    public double division(int a, int b) {
+        return a / b;
+    }
+
+    public void timeout(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(int respuesta) {
+        this.respuesta = respuesta;
+    }
+
+}
